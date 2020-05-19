61ccc15551d8f4adc048f9d5689f930ae36e2c08
JonathanVormayr <61434821+JonathanVormayr@users.noreply.github.com>
2020-05-19 16:26:30 +0200

    init mongo
    Co-Authored-By: sebastianlindinger <sebastianlindinger@users.noreply.github.com>

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,40 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var mongo = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
 
-var content = fs.readFileSync('erfassungen.json');
-var jsonErfassungen = JSON.parse(content);
+
+var content = fs.readFileSync('gemeinden.json');
+var jsonGemeinden = JSON.parse(content);
+
+function gemeinden2Array(json) {
+	var result = [];
+	var keys = Object.keys(json);
+	keys.forEach(function (key) {
+		result.push(json[key]);
+	});
+	return result;
+}
+var gemeinden = gemeinden2Array(jsonGemeinden);
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("sunfinderDB");
+  var myobj = { postCode: "4902", name: "Wolfsegg am Hausruck"};
+  dbo.collection("TEST").insertMany(jsonGemeinden, function(err, res) {
+      if(err) throw err;
+      console.log(res);
+      db.close();
+  });
+});
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send(
+      gemeinden
+     );
 });
 
 app.listen(3000, function () {
