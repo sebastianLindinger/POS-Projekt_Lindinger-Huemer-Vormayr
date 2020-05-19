a4f7b724f920b858825c300a2781e265908307d8
sebastianLindinger <seiglmeg@gmail.com>
2020-05-19 23:41:41 +0200

    database and server in work

--- Server/server.js
+++ Server/server.js
@@ -7,38 +7,49 @@ var url = "mongodb://localhost:27017/";
 
 
 
-var content = fs.readFileSync('gemeinden.json');
-var jsonGemeinden = JSON.parse(content);
+var content = fs.readFileSync('municipalities.json');
+var jsonMunicipalities = JSON.parse(content);
 
-function gemeinden2Array(json) {
-	var result = [];
-	var keys = Object.keys(json);
-	keys.forEach(function (key) {
-		result.push(json[key]);
-	});
-	return result;
+function municipalitiesToArray(json) {
+    var result = [];
+    var keys = Object.keys(json);
+    keys.forEach(function(key) {
+        result.push(json[key]);
+    });
+    return result;
 }
-var gemeinden = gemeinden2Array(jsonGemeinden);
+var municipalities = municipalitiesToArray(jsonMunicipalities);
 
 MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("sunfinderDB");
-  
-  dbo.collection("l").find({}, {projection: {postCode: 0}}).toArray( function(err, result) {
     if (err) throw err;
-    console.log(result);
-    db.close();
-  });
+    var dbo = db.db("sunfinderDB");
 
- 
+    //nur beim 1. durchlauf
+    dbo.createCollection("municipalities", function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    });
+    dbo.collection("municipalities").insertMany(jsonMunicipalities);
+
+    //show everything
+    dbo.collection("municipalities").find({}).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        db.close();
+    });
+
+    //show id and postCode
+    dbo.collection("municipalities").find({}, {projection: {name: 0}}).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        db.close();
+    });
 });
 
-app.get('/', function (req, res) {
-  res.send(
-      gemeinden
-     );
+app.get('/', function(req, res) {
+    res.send(municipalities);
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
 });
