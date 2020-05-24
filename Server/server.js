b3d2c045ede56526413201cbc36fabd0f226cc4c
JonathanVormayr <61434821+JonathanVormayr@users.noreply.github.com>
2020-05-24 19:53:23 +0200

    server in development
    @sebastianlindinger

--- Server/server.js
+++ Server/server.js
@@ -4,41 +4,80 @@ var fs = require('fs');
 var mongo = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
+var fetch = require('node-fetch');
 
 
 
 var content = fs.readFileSync('gemeinden.json');
 var jsonGemeinden = JSON.parse(content);
+var dbo;
+var i = 0;
 
 function gemeinden2Array(json) {
-	var result = [];
-	var keys = Object.keys(json);
-	keys.forEach(function (key) {
-		result.push(json[key]);
-	});
-	return result;
+    var result = [];
+    var keys = Object.keys(json);
+    keys.forEach(function (key) {
+        result.push(json[key]);
+    });
+    return result;
 }
 var gemeinden = gemeinden2Array(jsonGemeinden);
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("sunfinderDB");
-  
-  dbo.collection("l").find({}, {projection: {postCode: 0}}).toArray( function(err, result) {
+MongoClient.connect(url, function (err, db) {
     if (err) throw err;
-    console.log(result);
-    db.close();
-  });
+    dbo = db.db("sunfinderDB");
 
- 
+    //beim ersten mal
+    //dbo.collection("gemeinden1").insertMany(jsonGemeinden, function (err, res) {
+    //    if (err) throw err;
+    //});
+
+    getWeatherData();
 });
 
+async function getWeatherData() {
+    var d = new Date();
+    var hours = d.getHours();
+    var urlAPI = 'http://api.openweathermap.org/data/2.5/weather?q=<municipality>&appid=e612c50567b28c47bd1e1d25d43fe21e';
+    var actualName;
+
+
+    if (hours <= 21 && hours >= 5) {
+        if (i = 1582) i = 0;
+
+        dbo.collection("gemeinden1").find({}).toArray(function (err, result) {
+            if (err) throw err;
+            console.log(result);
+
+            actualName = result[i].name;
+            urlAPI = urlAPI.replace("<municipality>", actualName);
+
+
+            fetch(urlAPI)
+                .then(response => response.json())
+                .then(data => {
+
+                    var myquery = { _id: i.toString() };
+                    var newvalues = { $set: { weatherData: JSON.stringify(data)} };
+                    dbo.collection("gemeinden1").updateOne(myquery, newvalues, function (err, res) {
+                        if (err) throw err;
+                    });
+
+                });
+
+            urlAPI = urlAPI.replace(actualName, "<municipality>");
+            i = i + 1;
+        });   
+    }
+    setTimeout(getWeatherData, 1870);
+}
+
 app.get('/', function (req, res) {
-  res.send(
-      gemeinden
-     );
+    res.send(
+        gemeinden
+    );
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
