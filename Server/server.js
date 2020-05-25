b50b9144dfdd1de5c6b93043d02da11e68a5ba23
sebastianLindinger <52959748+sebastianLindinger@users.noreply.github.com>
2020-05-25 20:21:15 +0200

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -5,6 +5,7 @@ var mongo = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/";
 var fetch = require('node-fetch');
+var sunPlaceArr = [];
 
 var content = fs.readFileSync('municipalities.json');
 var jsonMunicipalities = JSON.parse(content);
@@ -47,7 +48,7 @@ var i = 0;
 async function getWeatherData() {
     var date = new Date();
     var hours = date.getHours();
-    var urlAPI = 'http://api.openweathermap.org/data/2.5/weather?q=<name>&appid=e612c50567b28c47bd1e1d25d43fe21e';
+    var urlAPI = 'http://api.openweathermap.org/data/2.5/weather?q=<name>,at&appid=e612c50567b28c47bd1e1d25d43fe21e';
     var currentName;
 
     //update weatherData only between 5.00 a.m. to 9.59 p.m
@@ -56,7 +57,7 @@ async function getWeatherData() {
 
         dbo.collection(collectionName).find({}).toArray(function (err, result) {
             if (err) throw err;
-            console.log(result);
+            //console.log(result);
 
             currentName = result[i].name;
             urlAPI = urlAPI.replace("<name>", currentName);
@@ -66,7 +67,7 @@ async function getWeatherData() {
                 .then(response => response.json())
                 .then(data => {
                     var myQuery = { _id: i.toString() };
-                    var newValues = { $set: { weatherData: JSON.stringify(data) } };
+                    var newValues = { $set: { weatherData: data} };
                     dbo.collection(collectionName).updateOne(myQuery, newValues, function (err, res) {
                         if (err) throw err;
                     });
@@ -78,7 +79,7 @@ async function getWeatherData() {
     setTimeout(getWeatherData, 1870);
 }
 
-app.get('/', function (req, res) {
+app.get('/db', function (req, res) {
     dbo.collection(collectionName).find({}).toArray(function (err, result) {
         if (err) throw err;
         res.json(result);
@@ -86,6 +87,55 @@ app.get('/', function (req, res) {
 
 });
 
+app.get('/sunfinderGetData', function (req, res) {
+    var query = req.query;
+    var lat = query.lat;
+    var lon = query.lon;
+    var result = [];
+    
+    getSunnyPlaces(lat,lon);
+    res.send(sunPlaceArr);
+    
+});
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
+function getSunnyPlaces(lat, lon) {
+    dbo.collection(collectionName).find({}).toArray(function (err, result) {
+        sunPlaceArr = [];
+        if (err) throw err;
+        for(var  j= 0;j <= 1564;j++){
+            //console.log(result[j]);
+            var resultObject = result[j];
+            var gotWeather = resultObject.weatherData;
+            if(gotWeather.clouds.all <= 35){
+                var latJ = resultObject.weatherData.coord.lat;
+                var lonJ = resultObject.weatherData.coord.lon;
+            
+                var distance = getDistance(lat,lon,latJ,lonJ);
+                sunPlaceArr.push(resultObject);
+                
+            }
+         }
+         return 1;
+    });
+}
+
+function getDistance(lat1, lon1, lat2, lon2) {
+    var radlat1 = Math.PI * lat1/180
+    var radlat2 = Math.PI * lat2/180
+    var radlon1 = Math.PI * lon1/180
+    var radlon2 = Math.PI * lon2/180
+
+    var theta = lon1-lon2
+    var radtheta = Math.PI * theta/180
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    dist = Math.acos(dist)
+
+    dist = dist * 180/Math.PI
+    dist = dist * 60 * 1.1515
+    dist = dist * 1.609344 
+    return dist
+}
