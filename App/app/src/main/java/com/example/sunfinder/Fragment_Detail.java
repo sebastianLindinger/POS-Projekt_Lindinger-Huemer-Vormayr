57795db44dc5ce064908ff66e3dfe8dc2bd86450
Jhuemer17 <johannes.huemer@gmx.at>
2020-05-27 13:25:22 +0200

    Implemented MasterActivity to Detail- and FactsActivity connection

--- App/app/src/main/java/com/example/sunfinder/Fragment_Detail.java
+++ App/app/src/main/java/com/example/sunfinder/Fragment_Detail.java
@@ -10,11 +10,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 
-public class Fragment_Detail extends Fragment {
+public class Fragment_Detail extends Fragment implements View.OnClickListener {
     private TextView textView_yourTown;
     private TextView textView_clouds;
     private TextView textView_wind;
@@ -24,20 +25,43 @@ public class Fragment_Detail extends Fragment {
     private TextView textView_tempMin;
     private TextView textView_humidity;
     private TextView textView_pessure;
+    private Button button_viewFacts;
+    private Button button_openMap;
     private ListView listView;
     private static final String TAG = Fragment_Start.class.getSimpleName();
+    OnViewFactsListener mListener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         Log.d(TAG, "onCreateView: entered");
-        View view = inflater.inflate(R.layout.fragment_fragment__master, container, false);
+        View view = inflater.inflate(R.layout.fragment_fragment__detail, container, false);
         initializeViews(view);
         return view;
     }
     private void initializeViews(View view)
     {
+        button_viewFacts = view.findViewById(R.id.button_Detail_ViewFacs);
+        button_viewFacts.setOnClickListener(this);
         //implement this..
+    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnViewFactsListener) {
+            mListener = (OnViewFactsListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == button_viewFacts.getId())
+        {
+            mListener.viewFactsClicked();
+        }
     }
 }
---
+++ App/app/src/main/java/com/example/sunfinder/OnViewFactsListener.java
@@ -0,0 +1,5 @@
+package com.example.sunfinder;
+
+public interface OnViewFactsListener {
+    void viewFactsClicked();
+}
