c99602811760c1735ea31a0eff52d138b052f035
Jhuemer17 <johannes.huemer@gmx.at>
2020-05-27 11:26:17 +0200

    Implemented StartFragment to MainActivity conntection

--- App/app/src/main/java/com/example/sunfinder/MainActivity.java
+++ App/app/src/main/java/com/example/sunfinder/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.sunfinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnSunClickedListener{
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onSunClicked(Double lon, Double lat) {
+        Log.d(TAG, "onSunClicked: entered");
+        Log.d(TAG, "lon="+lon+" lat="+lat);
+        if(lon == null || lat == null)
+        {
+            Toast.makeText(this, "Sie müssen zuerst einen Ort angeben!", Toast.LENGTH_LONG).show();
+        }
+        else {
+            callMasterActivity();
+        }
+    }
+    private void callMasterActivity()
+    {
+        //MasterActivity aufrufen.
     }
 }
