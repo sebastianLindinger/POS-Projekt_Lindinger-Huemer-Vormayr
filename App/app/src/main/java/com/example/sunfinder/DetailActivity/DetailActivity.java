9045e1449a558dc475f008158c81fba8797292e2
Jhuemer17 <johannes.huemer@gmx.at>
2020-05-29 11:14:32 +0200

    Beautified some Layouts

--- App/app/src/main/java/com/example/sunfinder/DetailActivity/DetailActivity.java
+++ App/app/src/main/java/com/example/sunfinder/DetailActivity/DetailActivity.java
@@ -4,25 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.sunfinder.FactsActivity.FactsActivity;
 import com.example.sunfinder.FactsActivity.Fragment_Facts;
+import com.example.sunfinder.MainActivity.Fragment_Start;
 import com.example.sunfinder.R;
 
 public class DetailActivity extends AppCompatActivity implements OnViewFactsListener {
 
     private boolean showFacts = false;
     private Fragment_Facts fragment_facts;
+    private static final String TAG = DetailActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         fragment_facts = (Fragment_Facts)getSupportFragmentManager().findFragmentById(R.id.fragment_facts);
         showFacts = fragment_facts != null && fragment_facts.isInLayout();
+        if(showFacts){
+            viewFactsClicked();
+            findViewById(R.id.button_Detail_ViewFacs).setVisibility(View.GONE);
+        }
     }
     @Override
     public void viewFactsClicked() {
-        if (showFacts)fragment_facts.showinformation();
+        Log.d(TAG, "vieFactsClicked: entered");
+        if (showFacts){
+            fragment_facts.showinformation();
+        }
         else callFactsActivity();
     }
     private void callFactsActivity()
