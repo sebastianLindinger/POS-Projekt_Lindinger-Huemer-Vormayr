1889971674f5dfd6001cc04be44183a1add20aac
Jhuemer17 <johannes.huemer@gmx.at>
2020-05-29 10:12:36 +0200

    Updated DataStorage

--- App/app/src/main/java/com/example/sunfinder/DataAdministration/DataStorage.java
+++ App/app/src/main/java/com/example/sunfinder/DataAdministration/DataStorage.java
@@ -1,4 +1,11 @@
 package com.example.sunfinder.DataAdministration;
 
+import java.util.ArrayList;
+
 public class DataStorage {
+    ArrayList<City>cities;
+    public DataStorage(ArrayList<City>cities)
+    {
+        this.cities = cities;
+    }
 }
