85d10fc494bb568b0174368655aa20084db11a60
sebastianLindinger <seiglmeg@gmail.com>
2020-06-06 15:22:04 +0200

    updated master screen

--- App/app/src/main/java/com/example/sunfinder/DataAdministration/Functions.java
+++ App/app/src/main/java/com/example/sunfinder/DataAdministration/Functions.java
@@ -7,4 +7,11 @@ public class Functions {
         DecimalFormat df = new DecimalFormat("#.#");
         return Double.parseDouble(df.format(kelvin - 273.15).replace(",", "."));
     }
+
+    public static Weather getWeather(City city) {
+        if(city.weatherData.weather[0].icon.equals("01d") && city.weatherData.weather[0].id == 800) return Weather.SUN;
+        else if(city.weatherData.weather[0].main.equals("Rain")) return Weather.RAIN;
+        else if(city.weatherData.weather[0].main.equals("Thunderstorm")) return Weather.THUNDERSTORM;
+        else return Weather.CLOUD;
+    }
 }
